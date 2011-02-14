3a6b75461c6792eb9ed205cfa6b9e77235d390f8
mikeplavsky <Mike.Plavsky@quest.com>
2011-02-14 18:21:18 +0300

    fixed # issue

--- agent.js
+++ agent.js
@@ -8,7 +8,7 @@ function psconsole_notify(res) {
 ps.eval = function ( cmd ) {    
     
     var script = document.createElement('script');    
-    script.src = "http://" + ps.server + ":35/?cmd=" + cmd + "&callback=psconsole_notify&_=" + Math.floor( Math.random() * 1000000000 ).toString();
+    script.src = "http://" + ps.server + ":35/?cmd=" + escape(cmd) + "&callback=psconsole_notify&_=" + Math.floor( Math.random() * 1000000000 ).toString();
      
     document.body.appendChild( script );
     return "Running...";
